bc5e8df70476674b7fcc9f1b930c3bea9b4adae9
Kauê <kauevecchia@hotmail.com>
2025-03-12 22:00:28 -0300

    style: minor styling adjustments in DefaultLayout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export function DefaultLayout() {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col gap-24 items-center min-h-screen font-roboto bg-my-background leading-snug">
             <Header />
             <Outlet />
         </div>
