facef6b50db139466eddb282dd28931a8056b751
Kauê <kauevecchia@hotmail.com>
2025-03-14 23:15:51 -0300

    chore: import zustand and coffees images

---
+++ src/store/coffees.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+import American from '../assets/American.svg'
+import Arabic from '../assets/Arabic.svg'
+import Cappucino from '../assets/HotChocolate.svg'
+import CoffeeWithMilk from '../assets/CoffeeWithMilk.svg'
+import ColdCoffee from '../assets/ColdCoffee.svg'
+import CreamyExpresso from '../assets/CreamyExpresso.svg'
+import Cuban from '../assets/Cuban.svg'
+import Hawaiian from '../assets/Hawaiian.svg'
+import HotChocolate from '../assets/HotChocolate.svg'
+import Irish from '../assets/Irish.svg'
+import Latte from '../assets/Latte.svg'
+import Macchiato from '../assets/Macchiato.svg'
+import Mochaccino from '../assets/v.svg'
+import TraditionalExpresso from '../assets/TraditionalExpresso.svg'
+
