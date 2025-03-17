a601f7933901bcc7df3a00dc91dad55c9319d12c
Kauê <kauevecchia@hotmail.com>
2025-03-16 22:31:59 -0300

    feat: add coffee section with dynamic card rendering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
-import { HeroSection } from "../components/HeroSection";
+import { Card } from '../components/Card'
+import { HeroSection } from '../components/HeroSection'
+import { coffees } from '../store/coffeesData'
 
 export function Home() {
-    return (
-        <main>
-            <HeroSection />
+  return (
+    <main>
+      <HeroSection />
 
-
-        </main>
-    )
+      <section className="mt-24 max-w-7xl mx-auto">
+        <h2 className="font-baloo font-extrabold text-2xl">Nossos cafés</h2>
+        <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-10 mt-14">
+          {coffees.map((coffee) => (
+            <Card coffeeData={coffee} />
+          ))}
+        </div>
+      </section>
+    </main>
+  )
 }
-
