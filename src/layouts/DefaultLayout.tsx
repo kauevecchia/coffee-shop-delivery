a2265bbab52e439a2e369e162fdca22b20458e94
Kauê <kauevecchia@hotmail.com>
2025-03-16 23:06:12 -0300

    style: add padding bottom to DefaultLayout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export function DefaultLayout() {
     return (
-        <div className="flex flex-col gap-24 items-center min-h-screen font-roboto bg-my-background leading-snug">
+        <div className="flex flex-col gap-24 items-center min-h-screen font-roboto bg-my-background leading-snug pb-32">
             <Header />
             <Outlet />
         </div>
