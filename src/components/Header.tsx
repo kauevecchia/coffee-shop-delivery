44d9f1ae398eedcbfc3a02fb8f5995196c8f495a
Kauê <kauevecchia@hotmail.com>
2025-03-16 23:03:25 -0300

    feat: display cart item count in Header using zustand

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 import Logo from '../../public/Logo.svg'
 import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+import { useCoffeeStore } from '../store/coffeeStore'
 
 export function Header() {
+    const { coffeesInCart } = useCoffeeStore()
+    const cartItems = coffeesInCart()
+
     return (
         <header className='flex justify-between items-center p-8 w-full'>
             <div className='max-w-7xl w-full mx-auto flex items-center justify-between'>
@@ -12,8 +16,11 @@ export function Header() {
                         <MapPin size={22} weight='fill' className='text-my-purple'/>
                         <span className='text-purple-dark'>Porto Alegre, RS</span>
                     </div>
-                    <div className='flex items-center justify-center bg-yellow-light p-2 rounded-md'>
-                        <ShoppingCart size={22} weight='fill' className='text-my-yellow cursor-pointer'/>
+                    <div className='relative flex items-center justify-center bg-yellow-light p-2 rounded-md'>
+                        <div className='absolute flex items-center justify-center right-0 top-0 rounded-full -translate-y-2 translate-x-2 bg-yellow-dark w-5 h-5 text-white font-bold text-xs'>
+                            {cartItems.length}
+                        </div>
+                        <ShoppingCart size={22} weight='fill' className='text-yellow-dark cursor-pointer'/>
                     </div>
                 </div>
             </div>
